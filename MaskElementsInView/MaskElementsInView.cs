036858c996193befc196a379df2113458d8fa651
Kobe Vandenberghe <Kobe.Vandenberghe@student.howest.be>
2025-06-30 12:10:57 +0200

    change

--- MaskElementsInView/MaskElementsInView.cs
+++ MaskElementsInView/MaskElementsInView.cs
@@ -56,7 +56,7 @@ namespace MaskElementsInView
 
 			foreach (Element element in elements)
 			{
-				element.Mask("ReasonForMasking", 10);
+				element.Mask("ReasonForMasking", 20);
 			}
 		}
     }
